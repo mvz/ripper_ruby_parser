fd8b7d3fc0223ef715f646ad5aab4f3829ecf953
Matijs van Zuijlen <matijs@matijs.net>
2012-02-29 09:34:02 +0100

    Make main parser post-process its output with the passed sexp processor.

--- lib/ripper_ruby_parser/parser.rb
+++ lib/ripper_ruby_parser/parser.rb
@@ -3,8 +3,13 @@ require 'ripper_ruby_parser/sexp_processor'
 
 module RipperRubyParser
   class Parser
+    def initialize processor=SexpProcessor.new
+      @processor = processor
+    end
+
     def parse source
-      Sexp.from_array Ripper.sexp source
+      exp = Sexp.from_array(Ripper.sexp source)
+      @processor.process exp
     end
   end
 end
--- test/unit/parser_test.rb
+++ test/unit/parser_test.rb
@@ -6,5 +6,16 @@ describe RipperRubyParser::Parser do
       result = RipperRubyParser::Parser.new.parse "foo"
       result.must_be_instance_of Sexp
     end
+
+    it "post-processes its result with the passed sexp processor" do
+      sexp_p = MiniTest::Mock.new
+      sexp_p.expect :process, s(:result), [Sexp]
+
+      parser = RipperRubyParser::Parser.new sexp_p
+      result = parser.parse "any code"
+
+      result.must_equal s(:result)
+      sexp_p.verify
+    end
   end
 end
