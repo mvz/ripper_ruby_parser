f9c77a47d419bd7771079132f7cf10615e7faae4
Matijs van Zuijlen <matijs@matijs.net>
2012-02-29 12:54:49 +0100

    Add more complex end-to-end test.

--- test/end_to_end/comparison_test.rb
+++ test/end_to_end/comparison_test.rb
@@ -22,5 +22,35 @@ describe "Using RipperRubyParser and RubyParser" do
       imitation.must_equal original
     end
   end
+
+  describe "for a more complex program" do
+    let :program do
+      <<-END
+      module Quux
+        class Foo
+          def bar
+            baz = 3
+            qux baz
+          end
+          def qux it
+            if it == 3
+              [1,2,3].map {|i| 2*i}
+            end
+          end
+        end
+      end
+
+      Quux::Foo.new.bar
+      END
+    end
+
+    it "gives the same result" do
+      original = oldparser.parse program
+      imitation = newparser.parse program
+
+      imitation.must_equal original
+    end
+  end
+
 end
 
