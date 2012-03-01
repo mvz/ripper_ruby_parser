7f3e49af472c1ad61436340400ae41ef74bde113
Matijs van Zuijlen <matijs@matijs.net>
2012-03-01 12:01:54 +0100

    Add end-to-end test that parses ripper_ruby_parser itself.

---
+++ test/end_to_end/lib_comparison_test.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
+require 'ruby_parser'
+
+describe "Using RipperRubyParser and RubyParser" do
+  let :newparser do
+    RipperRubyParser::Parser.new
+  end
+
+  let :oldparser do
+    RubyParser.new
+  end
+
+  Dir.glob("lib/**/*.rb").each do |file|
+    describe "for #{file}" do
+      let :program do
+        File.read file
+      end
+
+      it "gives the same result" do
+        original = oldparser.parse program
+        imitation = newparser.parse program
+
+        imitation.must_equal original
+      end
+    end
+  end
+end
+
+
