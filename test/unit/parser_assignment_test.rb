5bcc2f2181298c4215fe613f713432f3c9e89ab6
Matijs van Zuijlen <matijs@matijs.net>
2012-04-01 14:54:36 +0200

    Add a passing test for multiple assigment.

--- test/unit/parser_assignment_test.rb
+++ test/unit/parser_assignment_test.rb
@@ -3,13 +3,21 @@ require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 describe RipperRubyParser::Parser do
   describe "#parse" do
     describe "for multiple assignment" do
-      specify {
+      specify do
         "foo, * = bar".
+          must_be_parsed_as s(:masgn,
+                              s(:array, s(:lasgn, :foo), s(:splat)),
+                              s(:to_ary, s(:call, nil, :bar, s(:arglist))))
+      end
+
+      specify do
+        "(foo, *bar) = baz".
           must_be_parsed_as s(:masgn,
                               s(:array,
                                 s(:lasgn, :foo),
-                                s(:splat)),
-                                s(:to_ary, s(:call, nil, :bar, s(:arglist)))) }
+                                s(:splat, s(:lasgn, :bar))),
+                              s(:to_ary, s(:call, nil, :baz, s(:arglist))))
+      end
     end
   end
 end
