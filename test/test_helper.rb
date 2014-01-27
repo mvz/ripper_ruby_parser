42f7331795246efe936af4b32cde83ac897d5686
Matijs van Zuijlen <matijs@matijs.net>
2014-01-27 21:19:05 +0100

    Avoid minitest warning

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ if ENV["SIMPLECOV"]
   require 'simplecov'
   SimpleCov.start
 end
-require 'minitest/spec'
 require 'minitest/autorun'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
