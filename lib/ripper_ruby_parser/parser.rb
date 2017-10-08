8ce3080d2e1c4a7c84a0be06076a2deb8515a9f5
Matijs van Zuijlen <matijs@matijs.net>
2017-10-08 12:02:13 +0200

    Remove extra_compatible flag

--- lib/ripper_ruby_parser/parser.rb
+++ lib/ripper_ruby_parser/parser.rb
@@ -5,19 +5,11 @@ module RipperRubyParser
   # Main parser class. Brings together Ripper and our
   # RipperRubyParser::SexpProcessor.
   class Parser
-
-    # @api private
-    attr_accessor :extra_compatible
-
-    def initialize
-      @extra_compatible = false
-    end
-
     def parse(source, filename = '(string)', lineno = 1)
       parser = CommentingRipperParser.new(source, filename, lineno)
       exp = parser.parse
 
-      processor = SexpProcessor.new(filename: filename, extra_compatible: extra_compatible)
+      processor = SexpProcessor.new(filename: filename)
       result = processor.process exp
 
       if result == s(:void_stmt)
