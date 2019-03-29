mvz/ripper_ruby_parser

commit 5efd19d2e95ed20923b7433c03ca1ae4bda4dfb7

Improve compatibility parsing carriage returns in heredocs
RubyParser basically throws out all carriage returns, so do that when
running in extra-compatible mode.

lib/ripper_ruby_parser/sexp_handlers/literals.rb
@@ -256,7 +256,11 @@ module RipperRubyParser
       def handle_string_unescaping(content, delim)
         case delim
         when INTERPOLATING_HEREDOC, *INTERPOLATING_STRINGS
-          unescape(content)
+          if extra_compatible
+            unescape(content).delete("\r")
+          else
+            unescape(content)
+          end
         when INTERPOLATING_WORD_LIST
           unescape_wordlist_word(content)
         when *NON_INTERPOLATING_STRINGS

test/ripper_ruby_parser/sexp_handlers/literals_test.rb
@@ -599,6 +599,16 @@ describe RipperRubyParser::Parser do
             must_be_parsed_as s(:str, "bar\tbaz\n")
         end
 
+        it 'converts \r to carriage returns' do
+          "<<FOO\nbar\\rbaz\\r\nFOO".
+            must_be_parsed_as s(:str, "bar\rbaz\r\n")
+        end
+
+        it 'removes \r in extra-compatible mode' do
+          "<<FOO\nbar\\rbaz\\r\nFOO".
+            must_be_parsed_as s(:str, "barbaz\n"), extra_compatible: true
+        end
+
         it 'does not unescape with single quoted version' do
           "<<'FOO'\nbar\\tbaz\nFOO".
             must_be_parsed_as s(:str, "bar\\tbaz\n")

test/samples/strings.rb
@@ -37,6 +37,16 @@ FOO
 \n
 FOO
 
+# Escape sequences in heredocs, in particular carriage returns
+#
+<<FOO
+foo\rbar\tbaz\r
+FOO
+
+<<'FOO'
+foo\rbar\tbaz\r
+FOO
+
 # Line continuation
 "foo\
 bar"

commit 3c4726d45ae87c35e83191be03464e51225b5e89

Restore handling of carriage returns for double-quoted strings
Handling carriage-returns in double-quoted strings was broken in commit
5efd19d2e95ed20923b7433c03ca1ae4bda4dfb7. This commit adds integration
tests and fixes the problem.

lib/ripper_ruby_parser/sexp_handlers/literals.rb
@@ -255,12 +255,14 @@ module RipperRubyParser
 
       def handle_string_unescaping(content, delim)
         case delim
-        when INTERPOLATING_HEREDOC, *INTERPOLATING_STRINGS
+        when INTERPOLATING_HEREDOC
           if extra_compatible
             unescape(content).delete("\r")
           else
             unescape(content)
           end
+        when *INTERPOLATING_STRINGS
+          unescape(content)
         when INTERPOLATING_WORD_LIST
           unescape_wordlist_word(content)
         when *NON_INTERPOLATING_STRINGS

test/ripper_ruby_parser/sexp_handlers/literals_test.rb
@@ -604,11 +604,6 @@ describe RipperRubyParser::Parser do
             must_be_parsed_as s(:str, "bar\rbaz\r\n")
         end
 
-        it 'removes \r in extra-compatible mode' do
-          "<<FOO\nbar\\rbaz\\r\nFOO".
-            must_be_parsed_as s(:str, "barbaz\n"), extra_compatible: true
-        end
-
         it 'does not unescape with single quoted version' do
           "<<'FOO'\nbar\\tbaz\nFOO".
             must_be_parsed_as s(:str, "bar\\tbaz\n")
@@ -1105,6 +1100,17 @@ describe RipperRubyParser::Parser do
       it 'works for rationals' do
         '1000r'.
           must_be_parsed_as s(:lit, 1000r)
+      end
+    end
+
+    describe 'in extra compatible mode' do
+      it 'converts \r to carriage returns in double-quoted strings' do
+        '"foo\\rbar"'.must_be_parsed_as s(:str, "foo\rbar"), extra_compatible: true
+      end
+
+      it 'removes \r from heredocs' do
+        "<<FOO\nbar\\rbaz\\r\nFOO".
+          must_be_parsed_as s(:str, "barbaz\n"), extra_compatible: true
       end
     end
   end

test/samples/strings.rb
@@ -6,6 +6,7 @@
 %w(foo\nbar baz)
 
 "foo\u273bbar"
+"foo\a\b\e\f\r\s\t\vbar\nbaz"
 "\0"
 "foo#{bar}\0"
 "foo#{bar}baz\0"
@@ -13,6 +14,7 @@
 "#{foo}2\302\275"
 %W(2\302\275)
 /2\302\275/
+
 
 # Encoding
 "日本語"