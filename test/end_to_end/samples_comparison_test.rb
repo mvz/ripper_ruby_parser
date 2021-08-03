092752fdcae2253d55554e9b370665f4f8a7c8fd
Matijs van Zuijlen <matijs@matijs.net>
2021-08-03 09:48:35 +0200

    Add Ruby 3.0 argument forwarding sample

--- test/end_to_end/samples_comparison_test.rb
+++ test/end_to_end/samples_comparison_test.rb
@@ -7,6 +7,7 @@ describe "Using RipperRubyParser and RubyParser" do
   Dir.glob(File.expand_path("../samples/*.rb", File.dirname(__FILE__))).each do |file|
     next if RUBY_VERSION < "2.6.0" && file.match?(/_26.rb\Z/)
     next if RUBY_VERSION < "2.7.0" && file.match?(/_27.rb\Z/)
+    next if RUBY_VERSION < "3.0.0" && file.match?(/_30.rb\Z/)
 
     it "gives the same result for #{file}" do
       program = File.read file
