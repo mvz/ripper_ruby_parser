ee3e9a46f700e51e288ab19c104cf2ba1e5009d6
Matijs van Zuijlen <matijs@matijs.net>
2021-09-18 12:09:03 +0200

    Improve diffs when sample comparison tests fail

--- test/end_to_end/samples_comparison_test.rb
+++ test/end_to_end/samples_comparison_test.rb
@@ -4,6 +4,8 @@ require File.expand_path("../test_helper.rb", File.dirname(__FILE__))
 require "ruby_parser"
 
 describe "Using RipperRubyParser and RubyParser" do
+  make_my_diffs_pretty!
+
   Dir.glob(File.expand_path("../samples/*.rb", File.dirname(__FILE__))).each do |file|
     next if RUBY_VERSION < "2.6.0" && file.match?(/_26.rb\Z/)
     next if RUBY_VERSION < "2.7.0" && file.match?(/_27.rb\Z/)
