fb61fb89c72a1215e4e868c4efa2ba94ef7dc17f
Matijs van Zuijlen <matijs@matijs.net>
2023-06-10 11:14:12 +0200

    Add passing assignment samples
    This simpler type of assignment was not yet part of the end-to-end test
    suite.

--- test/samples/assignment.rb
+++ test/samples/assignment.rb
@@ -23,6 +23,18 @@ foo.bar ||= baz(qux)
 foo.bar ||= baz.qux quuz
 foo.bar ||= baz.qux(quuz)
 
+foo = baz
+foo += baz
+foo += baz qux
+foo += baz(qux)
+foo += baz.qux quuz
+foo += baz.qux(quuz)
+foo ||= baz
+foo ||= baz qux
+foo ||= baz(qux)
+foo ||= baz.qux quuz
+foo ||= baz.qux(quuz)
+
 # Destructuring assignments
 foo, bar = baz
 (foo, bar) = baz
